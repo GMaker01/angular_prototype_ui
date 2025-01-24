1896cc6505d2161d28aba07fd0831deaa5482874
Shubham Sharma <shubhamsharma401@gmail.com>
2025-01-24 21:19:08 +0530

    Init

--- src/app/pages/sub-categories/sub-categories.component.ts
+++ src/app/pages/sub-categories/sub-categories.component.ts
@@ -10,9 +10,9 @@ import { DataService } from 'src/app/shared/data.service';
 export class SubCategoriesComponent implements OnInit {
 
     name = '';
-    categoryName: '';
-    data: any;
-    subCategories: any[];
+    categoryName = '';
+    data: any = [];
+    subCategories: any[] = [];
     constructor(private router: Router, private route: ActivatedRoute, private dataService: DataService) {
         this.data = dataService.getData();
     }
@@ -23,9 +23,9 @@ export class SubCategoriesComponent implements OnInit {
             if (params && params.categoryName && params.name) {
                 this.categoryName = params.categoryName;
                 this.name = params.name;
-                const branches = [].concat.apply([], this.data.locations.map(x => (x.branches)));
-                const categories = [].concat.apply([], branches.map(x => (x.categories)));
-                const selectedCategories = categories.find(x => x.name === this.categoryName);
+                const branches = [].concat.apply([], this.data.locations.map((x: any) => (x.branches)));
+                const categories = [].concat.apply([], branches.map((x: any) => (x.categories)));
+                const selectedCategories = categories.find((x: any) => x.name === this.categoryName);
                 if (selectedCategories) {
                     this.subCategories = selectedCategories.subcategories;
                 } else {
